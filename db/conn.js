7ccb91d8b372b990e90b3b65bc25b63266040140
santhi999 <nsanthipriya999@gmail.com>
2026-06-26 22:30:47 -0400

    nine

--- db/conn.js
+++ db/conn.js
@@ -1,13 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 async function connectDB(){
 	try {
-		await mongoose.connect('mongodb://nsanthipriya9_db_user:temp123@ac-otj27m1-shard-00-00.5xkujx3.mongodb.net:27017,ac-otj27m1-shard-00-01.5xkujx3.mongodb.net:27017,ac-otj27m1-shard-00-02.5xkujx3.mongodb.net:27017/sample_training?ssl=true&replicaSet=atlas-5icqdw-shard-0&authSource=admin&appName=MongoPractice')
+		//console.log("MONGO_URI:", process.env.MONGO_URI);
+		await mongoose.connect(process.env.MONGO_URI);
 		console.log("MongoDB Connected with Mongoose!")
 	} catch(e) {
 		console.error(e)
 	}
 }
 
-export default connectDB
+export default connectDB;
 
